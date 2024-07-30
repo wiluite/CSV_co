8a1b84d1750583369206ffe1da1636ff26faa57e
wiluite <tetasoft@yandex.ru>
2024-07-30 15:12:17 +0000

    added skeletons for new utilities

---
+++ utils/csvkit/csvsql.cpp
@@ -0,0 +1,33 @@
+//
+// Created by wiluite on 7/30/24.
+//
+#include <soci/soci.h>
+#if defined (SOCI_HAVE_SQLITE3)
+#include <soci/sqlite3/soci-sqlite3.h>
+#endif
+
+#if defined (SOCI_HAVE_EMPTY)
+#include <soci/empty/soci-empty.h>
+#endif
+
+#if defined (SOCI_HAVE_POSTGRESQL)
+#include <soci/postgresql/soci-postgresql.h>
+#endif
+
+#if defined(SOCI_HAVE_MYSQL)
+#include <soci/mysql/soci-mysql.h>
+#endif
+
+#if defined(SOCI_HAVE_FIREBIRD)
+#include <soci/firebird/soci-firebird.h>
+#endif
+
+#include <iostream>
+
+int main() {
+    try {
+        using namespace soci;
+    } catch (soci::soci_error const &err) {
+        std::cout << "ERROR!\n";
+    }
+}
