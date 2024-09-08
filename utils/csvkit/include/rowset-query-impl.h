ed6eca13d1fa43fce1a6cbb947d13c1192cd997e
wiluite <tetasoft@yandex.ru>
2024-09-08 12:36:38 +0300

    csvsql, sql2csv: --query correctly prints the doubles.

--- utils/csvkit/include/rowset-query-impl.h
+++ utils/csvkit/include/rowset-query-impl.h
@@ -5,11 +5,12 @@ namespace csvkit::cli::sql {
     void rowset_query(soci::session & sql, auto const & args, std::string const & q_s) {
         using namespace soci;
 
-        enum class yes_no_time {
+        enum class if_time {
             no = 0,
             yes
         };
-        std::map<unsigned, yes_no_time> col2time;
+        std::map<unsigned, if_time> col2time;
+
         rowset<row> rs = (sql.prepare << q_s);
 
         bool print_header = false;
@@ -33,8 +34,13 @@ namespace csvkit::cli::sql {
 
             auto print_data = [&](std::size_t i) {
                 column_properties const & props = rr.get_properties(i);
-                if (rr.get_indicator(i) == soci::i_null)
+                if (rr.get_indicator(i) == soci::i_null) {
+                    if (rr.size() == 1)
+                        std::cout << R"("")";
                     return;
+                }
+
+                num_stringstream ss("C");
 
                 switch(props.get_db_type())
                 {
@@ -42,21 +48,22 @@ namespace csvkit::cli::sql {
                         std::cout << rr.get<std::string>(i);
                         break;
                     case db_double:
-                        std::cout << rr.get<double>(i);
+                        ss << csv_mcv_prec(rr.get<double>(i));
+                        std::cout << ss.str();
                         break;
                     case db_int32:
                         std::cout << rr.get<int32_t>(i);
                         break;
                     case db_date:
                         d = rr.get<std::tm>(i);
-                        if (col2time[i] == yes_no_time::no and d.tm_hour == 0 and d.tm_min == 0 and d.tm_sec == 0) {
+                        if (col2time[i] == if_time::no and d.tm_hour == 0 and d.tm_min == 0 and d.tm_sec == 0) {
                             std::strftime(std::data(timeString_v2), std::size(timeString_v2),"%Y-%m-%d", &d);
                             std::cout << timeString_v2;
                         } else {
                             std::strftime(std::data(timeString_v1), std::size(timeString_v1),"%Y-%m-%d %H:%M:%S", &d);
                             std::cout << timeString_v1;
-                            if (col2time[i] == yes_no_time::no)
-                                col2time[i] = yes_no_time::yes;
+                            if (col2time[i] == if_time::no)
+                                col2time[i] = if_time::yes;
                         }
                         break;
                     default:
@@ -82,6 +89,7 @@ namespace csvkit::cli::sql {
                 sql.once << q_s,into (v);
                 if (!v.size())
                     return;  // we had supposed it was a "select", but it was another statement!
+
                 if (args.linenumbers)
                     std::cout << "line_number" << ',';
                 column_properties const &prop = v.get_properties(0);
