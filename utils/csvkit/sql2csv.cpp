90eba8a6a1e4d2428cd5773afc575f14a15e0a11
wiluite <tetasoft@yandex.ru>
2024-09-29 19:34:33 +0300

    Get rid of unnecessary inclusions.

--- utils/csvkit/sql2csv.cpp
+++ utils/csvkit/sql2csv.cpp
@@ -2,25 +2,6 @@
 // Created by wiluite on 7/30/24.
 //
 #include <soci/soci.h>
-#if defined (SOCI_HAVE_SQLITE3)
-#include <soci/sqlite3/soci-sqlite3.h>
-#endif
-
-#if defined (SOCI_HAVE_POSTGRESQL)
-#include <soci/postgresql/soci-postgresql.h>
-#endif
-
-#if defined(SOCI_HAVE_MYSQL)
-#include <soci/mysql/soci-mysql.h>
-#endif
-
-#if defined(SOCI_HAVE_FIREBIRD)
-#include <soci/firebird/soci-firebird.h>
-#endif
-
-#if defined(SOCI_HAVE_MARIADB)
-#include <soci/mariadb/soci-mariadb.h>
-#endif
 
 #include <ocilib.hpp>
 
