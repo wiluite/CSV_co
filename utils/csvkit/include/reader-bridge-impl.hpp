699100761f65431cc6fe594f4cfb739e8704aeb2
wiluite <tetasoft@yandex.ru>
2024-11-12 21:13:50 +0300

    Add a TODO.

--- utils/csvkit/include/reader-bridge-impl.hpp
+++ utils/csvkit/include/reader-bridge-impl.hpp
@@ -13,7 +13,7 @@
 #include "../external/date/date.h"
 #include <codecvt>
 #include "../external/Alphabet-AB/UtfConv.c"
-
+//TODO. Please distinct 1 as bool from 1.0 as double (0 from 0.0 as well)
 namespace csv_co::csvkit {
     constexpr auto to_basic_string_32(auto && str) {
         return std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t>().from_bytes(str);
